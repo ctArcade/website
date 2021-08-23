05975df21d8b6f34a23d3a024810a63a5c249a04
Brian Longmore <brianl.ext@gmail.com>
2021-08-22 23:57:23 -1000

    cleaned up style a lil

--- components/Header.js
+++ components/Header.js
@@ -15,14 +15,14 @@ export default function Header() {
    ));
 
    return (
-      <div className="w-full py-14 flex-col lg:flex-row flex place-content-between mb-10 md:mb-14 lg:mb-20">
+      <div className="w-full py-14 flex-col flex place-content-between mb-10 lg:mb-20 lg:flex-row md:mb-14">
          <Link href="/">
             <div className="flex justify-center cursor-pointer">
                <Image src="/assets/logoTransparent.svg" alt="ctArcade logo" width="130" height="130" />
             </div>
          </Link>
 
-         <div className="flex md:space-y-0 lg:space-y-0 space-y-2 mx-auto lg:m-0 md:space-x-12 items-center font-semibold flex-col md:flex-row">
+         <div className="flex whitespace-nowrap items-center flex-col space-y-2 mx-auto lg:space-y-0 lg:m-0 sm:space-x-12 sm:flex-row">
             {navs}
          </div>
       </div >
